14ddfa1b0caea943017e91f989e8438d461675f4
Wadie Mendja <mendja2014@gmail.com>
2023-09-12 09:43:14 +0100

    commit

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,10 +12,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        WebView webView;
-        webView = (WebView)findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(R.layout.splash_screen);
+        final long DELAY_MILLIS = 500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+                WebView webView;
+                webView = (WebView)findViewById(R.id.webView);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.loadUrl("https://azhaanexpress.web.app/login");
+            }
+        }, DELAY_MILLIS);
+
     }
 }
