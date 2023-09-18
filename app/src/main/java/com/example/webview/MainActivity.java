1de51c96befb54a995395c0d52463e184c69c122
Wadie Mendja <mendja2014@gmail.com>
2023-09-18 13:57:23 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +22,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 setContentView(R.layout.activity_main);
-                WebView webView;
+                final WebView webView;
                 webView = (WebView)findViewById(R.id.webView);
-                webView.getSettings().setJavaScriptEnabled(true);
+                WebSettings webSettings = webView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                webSettings.setDomStorageEnabled(true);
+                /* not necessary */
+                webView.setWebChromeClient(new WebChromeClient());
+                webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+                webSettings.setPluginState(WebSettings.PluginState.ON_DEMAND);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    webSettings.setAllowUniversalAccessFromFileURLs(true);
+                }
+                /* not necessary */
                 webView.loadUrl("https://azhaanexpress.web.app/login");
+                // Prevent opening URLs outside of the app
+                webView.setWebViewClient(new WebViewClient() {
+                    public boolean shouldOverrideUrlLoading(WebView view, String url){
+                        view.loadUrl(url);
+                        return false;
+                    }
+                });
             }
         }, DELAY_MILLIS);
 
+    }
+    @Override
+    public void onBackPressed(){
+        WebView wv = (WebView)findViewById(R.id.webView);
+        if(wv.canGoBack()){
+            wv.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
